3a357685c6d81c94b62c56a9c8527c0352707121
Kir Shatrov <shatrov@me.com>
2015-01-15 18:34:00 +0100

    Another approach to fixtures

--- test/rails-perf/build_jumper_test.rb
+++ test/rails-perf/build_jumper_test.rb
@@ -31,6 +31,8 @@ class TestBuildJumper < Minitest::Test
     assert_equal 1, RailsPerf.storage.builds.count
 
     assert jumper.build_id.present?
+
+    build = RailsPerf.storage.find_build(jumper.build_id)
   end
 
   def test_not_existing_build
@@ -61,12 +63,16 @@ class TestBuildJumper < Minitest::Test
   def stub_github_api
     stub_request(:get, "https://api.github.com/repos/rails/rails/commits/850159bd2c5e1e108d0256dd05424bbbf7926b59").
       with(:headers => {'Accept'=>'application/vnd.github.v3+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Octokit Ruby Gem 3.5.2'}).
-      to_return(:status => 200, :body => "", :headers => {})
+      to_return(:status => 200, :body => commit_body, :headers => {'Content-Type'=>'application/json'})
   end
 
   def stub_not_existing_github_api
     stub_request(:get, "https://api.github.com/repos/rails/rails/commits/850159bd2c5e1e108d0256dd05424bbbf7926b59").
       with(:headers => {'Accept'=>'application/vnd.github.v3+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Octokit Ruby Gem 3.5.2'}).
       to_return(:status => 404, :body => "", :headers => {})
+  end
+
+  def commit_body
+    File.open(fixture_path('commit.json')).read
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,3 +12,10 @@ require 'pry'
 # Add support to load paths
 $:.unshift File.expand_path('../support', __FILE__)
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+class Minitest::Test
+  def fixture_path(fixture)
+    current = Pathname.new(File.dirname(__FILE__))
+    current.join("fixtures", fixture)
+  end
+end
